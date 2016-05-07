93235974ebdd5d9fbd2ad7918945ae6050149eab
akaija <a.r.kaija@gmail.com>
2016-05-06 22:23:54 -0400

    renamed hstohm.py to run.py

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python
+# /usr/bin/env python
 
 import sys
+
 
 from htsohm.htsohm import htsohm
 
 if __name__ == "__main__":
+
     htsohm(int(sys.argv[1]),
            int(sys.argv[2]),
            float(sys.argv[3]),
