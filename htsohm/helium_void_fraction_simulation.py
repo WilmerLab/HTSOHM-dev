1ae2105cfe7660ae5fe5aeb20b7559db694eb690
akaija <a.r.kaija@gmail.com>
2016-05-09 18:54:37 -0400

    fixed dummy_test() in htsohm/simulate.py: switch from material_id to primary key, added calls to htsohm/*_simulations.py

--- htsohm/helium_void_fraction_simulation.py
+++ htsohm/helium_void_fraction_simulation.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # standard library imports
 import os
 import shutil
@@ -24,31 +23,6 @@ def write_raspa_file(filename, run_id, material_id):
             "            MoleculeDefinition\t\tTraPPE\n" +
             "            WidomProbability\t\t1.0\n" +
             "            CreateNumberOfMolecules\t0\n")
-=======
-import os
-import subprocess
-import shutil
-
-def write_raspa_file(filename, run_id, material_id):
-    with open(filename, "w") as config:
-        config.write("SimulationType\t\t\tMonteCarlo\n" +
-                        "NumberOfCycles\t\t\t1000\n" +             # number of MonteCarlo cycles
-                        "PrintEvery\t\t\t100\n" +
-                        "PrintPropertiesEvery\t\t100\n" +
-                        "\n" +
-                        "Forcefield\t\t\t%s-%s\n" % (run_id, material_id) +
-                        "CutOff\t\t\t\t12.8\n" +                       # LJ interaction cut-off, Angstroms
-                        "\n" +
-                        "Framework 0\n" +
-                        "FrameworkName %s-%s\n" % (run_id, material_id) +
-                        "UnitCells 1 1 1\n" +
-                        "ExternalTemperature 298.0\n" +       # External temperature, K
-                        "\n" +
-                        "Component 0 MoleculeName\t\thelium\n" +
-                        "            MoleculeDefinition\t\tTraPPE\n" +
-                        "            WidomProbability\t\t1.0\n" +
-                        "            CreateNumberOfMolecules\t0\n" )
->>>>>>> ec5f68c... separated out simulations into their own files that are independently testable and don't require the database. Handle the database in the run_all_simulations method
 
 def parse_output(output_file):
     results = {}
@@ -57,7 +31,6 @@ def parse_output(output_file):
             if not "Average Widom Rosenbluth-weight:" in line:
                 continue
             try:
-<<<<<<< HEAD
                 results['VF_val'] = float(line.split()[4])
             except IndexError:
                 print()
@@ -74,32 +47,7 @@ def run(run_id, material_id):
     output_dir = os.path.join('output', 'Output', 'System_0')
     filename = "output_%s-%s_1.1.1_298.000000_0.data" % (run_id, material_id)
     output_file = os.path.join(output_dir, filename)
-=======
-                results['VF_val'] = float(line.split()[4][:-1])
-            except IndexError:
-                print()
-
-    print( "\nVOID FRACTION :   %s\n" % (results['VF_val']) )
-    return results
-
-def run(run_id, material_id):
-    #
-    os.makedirs('output', exist_ok=True)
-    filename = "output/VoidFraction.input"
-    write_raspa_file(filename, run_id, material_id)
-    subprocess.run(['simulate', './VoidFraction.input'], check=True, cwd='output')
-
-    output_file = "output/Output/System_0/output_%s-%s_1.1.1_298.000000_0.data" % (run_id, material_id)
->>>>>>> ec5f68c... separated out simulations into their own files that are independently testable and don't require the database. Handle the database in the run_all_simulations method
     results = parse_output(output_file)
     shutil.rmtree("output")
 
     return results
-<<<<<<< HEAD
-=======
-    # Add to database...
-    #
-    # session.commit()
-    #
-    #
->>>>>>> ec5f68c... separated out simulations into their own files that are independently testable and don't require the database. Handle the database in the run_all_simulations method
--- htsohm/methane_loading_simulation.py
+++ htsohm/methane_loading_simulation.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # standard library imports
 import os
 import shutil
@@ -30,45 +29,11 @@ def write_raspa_file(filename, run_id, material_id, helium_void_fraction ):
             "            ReinsertionProbability\t1.0\n" +
             "            SwapProbability\t\t1.0\n" +
             "            CreateNumberOfMolecules\t0\n")
-=======
-import os
-import subprocess
-import shutil
-
-def write_raspa_file(filename, run_id, material_id, helium_void_fraction ):
-    with open(filename, "w") as config:
-        config.write("SimulationType\t\t\tMonteCarlo\n" +
-                        "NumberOfCycles\t\t\t1000\n" +             # number of MonteCarlo cycles
-                        "NumberOfInitializationCycles\t500\n" +    # number of initialization cycles
-                        "PrintEvery\t\t\t100\n" +
-                        "RestartFile\t\t\tno\n" +
-                        "\n" +
-                        "Forcefield\t\t\t%s-%s\n" % (run_id, material_id) +
-                        "ChargeMethod\t\t\tEwald\n"
-                        "CutOff\t\t\t\t12.0\n" +                   # electrostatic cut-off, Angstroms
-                        "\n" +
-                        "Framework 0\n" +
-                        "FrameworkName %s-%s\n" % (run_id, material_id) +
-                        "UnitCells 1 1 1\n" +
-                        "HeliumVoidFraction %s\n" % (helium_void_fraction) +
-                        "ExternalTemperature 298.0\n" +            # External temperature, K
-                        "ExternalPressure 3500000\n" +             # External pressure, Pa
-                        "\n" +
-                        "Component 0 MoleculeName\t\tmethane\n" +
-                        "            MoleculeDefinition\t\tTraPPE\n" +
-                        "            TranslationProbability\t1.0\n" +
-                        "            ReinsertionProbability\t1.0\n" +
-                        "            SwapProbability\t\t1.0\n" +
-                        "            CreateNumberOfMolecules\t0\n" )
->>>>>>> ec5f68c... separated out simulations into their own files that are independently testable and don't require the database. Handle the database in the run_all_simulations method
 
 def parse_output(output_file):
     results = {}
     with open(output_file) as origin:
-<<<<<<< HEAD
         line_counter = 1
-=======
->>>>>>> ec5f68c... separated out simulations into their own files that are independently testable and don't require the database. Handle the database in the run_all_simulations method
         for line in origin:
             if "absolute [mol/kg" in line:
                 results['ML_a_mk'] = line.split()[5]
@@ -82,7 +47,6 @@ def parse_output(output_file):
                 results['ML_e_cg'] = line.split()[6]
             elif "excess [cm^3 (STP)/c" in line:
                 results['ML_e_cc'] = line.split()[6]
-<<<<<<< HEAD
             elif "Average Host-Host energy:" in line:
                 host_host_line = line_counter + 8
             elif "Average Adsorbate-Adsorbate energy:" in line:
@@ -108,23 +72,14 @@ def parse_output(output_file):
                 results['host_adsorbate_cou'] = line.split()[7]
             line_counter += 1
     
-    print(
-        "\nMETHANE LOADING\tabsolute\texcess\n" +
-        "mol/kg\t\t%s\t%s\n" % (results['ML_a_mk'], results['ML_e_mk']) +
-        "cc/g\t\t%s\t%s\n"   % (results['ML_a_cg'], results['ML_e_cg']) +
-        "cc/cc\t\t%s\t%s\n"  % (results['ML_a_cc'], results['ML_e_cc']))
-=======
-    
     print("\nMETHANE LOADING\tabsolute\texcess\n" +
             "mol/kg\t\t%s\t%s\n" % (results['ML_a_mk'], results['ML_e_mk']) +
             "cc/g\t\t%s\t%s\n"   % (results['ML_a_cg'], results['ML_e_cg']) +
             "cc/cc\t\t%s\t%s\n"  % (results['ML_a_cc'], results['ML_e_cc']) )
->>>>>>> ec5f68c... separated out simulations into their own files that are independently testable and don't require the database. Handle the database in the run_all_simulations method
     
     return results
 
 def run(run_id, material_id, helium_void_fraction):
-<<<<<<< HEAD
     os.makedirs('output', exist_ok=True)
     filename = os.path.join('output', 'MethaneLoading.input')
     write_raspa_file(filename, run_id, material_id, helium_void_fraction)
@@ -134,20 +89,6 @@ def run(run_id, material_id, helium_void_fraction):
     filename = "output_%s-%s_1.1.1_298.000000_3.5e+06.data" % (run_id, material_id)
     filepath = os.path.join(output_dir, filename)
     results = parse_output(filepath)
-
-=======
-    # run_data = session.query(RunData).get(id)
-    
-    os.makedirs('output', exist_ok=True)
-    filename = 'output/MethaneLoading.input'
-    write_raspa_file(filename, run_id, material_id, helium_void_fraction)
-    subprocess.run(['simulate', './MethaneLoading.input'], check=True, cwd='output')
-    
-    output_file = "output/Output/System_0/output_%s-%s_1.1.1_298.000000_3.5e+06.data" % (run_id, material_id)
-    results = parse_output(output_file)
-
-    #STILL NEED TO GREP HEATDESORP
->>>>>>> ec5f68c... separated out simulations into their own files that are independently testable and don't require the database. Handle the database in the run_all_simulations method
     shutil.rmtree("output")
     
     return results
